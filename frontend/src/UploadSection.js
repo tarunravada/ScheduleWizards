3aecfdf22d4c5bdcb0a47c079b10e2a9fe622af0
Jay Bhatt <jbhatt7@uncc.edu>
2022-10-31 19:32:08 -0400

    Frontend basic

---
+++ frontend/src/UploadSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './uploadSection.css'
+
+function UploadSection() {
+  return (
+    <div className="uploadsection" id="ups">
+
+    </div>
+  )
+}
+
+export default UploadSection
